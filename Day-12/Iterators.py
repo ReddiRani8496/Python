1759617654368d9abd0e3d17641bffbf40836979
reddirani8496 <reddiranibattuluri@gmail.com>
2024-07-11 08:47:59 +0530

    iterators program uploaded..

---
+++ Day-12/Iterators.py
@@ -0,0 +1,51 @@
+# colors = ("red","black","white")
+
+# myiterator = iter(colors)
+
+# print(next(myiterator))
+# print(next(myiterator))
+
+
+# color = "black"
+
+# myiterator = iter(color)
+
+# print(next(myiterator))
+# print(next(myiterator))
+# print(next(myiterator))
+# print(next(myiterator))
+# print(next(myiterator))
+
+class MyNumbers:
+
+  def __iter__(self):
+
+    self.a = 1
+
+    return self
+
+
+
+  def __next__(self):
+
+    x = self.a
+
+    self.a += 1
+
+    return x
+
+
+myclass = MyNumbers()
+
+myiter = iter(myclass)
+
+print(next(myiter))
+
+print(next(myiter))
+
+print(next(myiter))
+
+
+
+
+
