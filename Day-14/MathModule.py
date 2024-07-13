5c6f1d1f3010b85689ad2b82606eea1679b2e79f
reddirani8496 <reddiranibattuluri@gmail.com>
2024-07-13 15:24:18 +0530

    math module programs uploaded..

---
+++ Day-14/MathModule.py
@@ -0,0 +1,19 @@
+import math
+
+max_value = max(1,2,3)
+
+print(max_value)
+
+min_value = min(1,2,3)
+
+print(min_value)
+
+print(pow(2,3))
+
+print(math.sqrt(min_value))
+
+print(math.ceil(5.6))
+
+print(math.floor(5.6))
+
+print(math.pi)
